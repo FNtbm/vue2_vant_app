c45823a8b7e4ffd213588ffb56f9e320cdc5aa75
WALL.E <1770957624@qq.com>
2022-09-14 16:06:11 +0800

    'modify fundSwipe'

--- src/components/fundPage/FundSwiper.vue
+++ src/components/fundPage/FundSwiper.vue
@@ -2,12 +2,16 @@
   <div>
     <van-swipe :autoplay="3000">
       <van-swipe-item v-for="(image, index) in images" :key="index">
-        <van-image :src="image" :alt="image" />
+        <img v-lazy="image" :alt="image" class="image" />
       </van-swipe-item>
     </van-swipe>
   </div>
 </template>
 <script>
+import Vue from "vue";
+import { Lazyload } from "vant";
+Vue.use(Lazyload);
+
 export default {
   data() {
     return {
@@ -21,7 +25,10 @@ export default {
 };
 </script>
 <style lang="less" scoped>
-.my-swipe img {
-  width: 375px;
+.van-swipe {
+  img {
+    width: 375px;
+    height: 187.5px;
+  }
 }
 </style>
